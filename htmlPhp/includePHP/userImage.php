eb2299464373dec55612e69ce5b99617fd9a74a3
BCWizard <puhj97009@gmail.com>
2023-05-12 23:23:09 +0800

    userImage package in userImage.php

---
+++ htmlPhp/includePHP/userImage.php
@@ -0,0 +1,19 @@
+<img src="
+            <?php
+              $conn=require_once "config.php";
+              $sql = "SELECT * FROM userInfo WHERE userAccount ='{$_SESSION["userAccount"]}'";
+              $result=mysqli_query($conn,$sql);                                                               //connection,query(查詢字串);回傳result
+              $data = mysqli_fetch_assoc($result);
+                                                //userInfo資料表裡是否有使用者圖片
+              if(!is_null($data["userImage"])){
+                $userImagePath = "./Manager/userImage/{$_SESSION['userAccount']}.jpg";
+                echo $userImagePath;
+              }else{                            //沒有則使用預設圖片
+                $userImagePath = "./Manager/userImage/default.jpg";
+                echo $userImagePath;
+              }
+            ?>
+            " class="img-fluid" alt="使用者圖片">
+            <?php
+              echo "Hi, {$_SESSION['userAccount']}";
+            ?>
