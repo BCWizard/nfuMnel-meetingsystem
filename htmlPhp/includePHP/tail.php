cbf7f0cb6c0bbaac770634543e9940d2a5e4403b
BCWizard <puhj97009@gmail.com>
2023-05-14 17:56:42 +0800

    zoom out tail

--- htmlPhp/includePHP/tail.php
+++ htmlPhp/includePHP/tail.php
@@ -1,4 +1,4 @@
-<div class="container">
+<div class="container footer">
     <div id="systemContactInfo">
         <div>具智慧化行為分析的會議系統</div>
         <div>聯絡資訊:MNELMeetingsystem@nfu.edu.tw</div>
--- htmlPhp/loginpage.php
+++ htmlPhp/loginpage.php
@@ -93,31 +93,31 @@ function function_alert($message) {
     </div>
 
     <div class="container">                                                               <!--max 1170px-->
-        <div class="row">
-          <div class="col-md-4">                                                          <!--992~1200px -> 使用4欄位-->    
-          </div>
-          <div class="col-md-4">
-            <form method="post" action="login.php" id="loginForm">
-                <div class="mb-3">
-                  <label for="Account" class="form-label">帳號</label>
-                  <input type="account" class="form-control" name = "account" id="account" placeholder="帳號 / Account">
-                </div>
-                <div class="mb-3">
-                  <label for="Password" class="form-label">密碼</label>
-                  <input type="password" class="form-control" name = "password" id="password" placeholder="密碼 / Password">
-                </div>
-                <div class="mb-3 form-check">
-                  <input type="checkbox" name = "setRememberLoginInfo" class="form-check-input" id="check">
-                  <label class="form-check-label" for="check">保持登入</label>
-                </div>
-                <div id="loginbtn">
-                  <button type="submit" class="btn btn-primary">登入 / Login</button>
-                </div>
-            </form>
-          </div>
-          <div class="col-md-4"> 
-          </div>
+      <div class="row">
+        <div class="col-md-4">                                                          <!--992~1200px -> 使用4欄位-->    
         </div>
+        <div class="col-md-4">
+          <form method="post" action="login.php" id="loginForm">
+              <div class="mb-3">
+                <label for="Account">帳號</label>
+                <input type="account" class="form-control" name = "account" id="account" placeholder="帳號 / Account">
+              </div>
+              <div class="mb-3">
+                <label for="Password">密碼</label>
+                <input type="password" class="form-control" name = "password" id="password" placeholder="密碼 / Password">
+              </div>
+              <div class="mb-3 form-check">
+                <input type="checkbox" name = "setRememberLoginInfo" class="form-check-input" id="check">
+                <label class="form-check-label" for="check">保持登入</label>
+              </div>
+              <div id="loginbtn">
+                <button type="submit" class="btn btn-primary">登入 / Login</button>
+              </div>
+          </form>
+        </div>
+        <div class="col-md-4"> 
+        </div>
+      </div>
     </div>
     
     <?php include("./includePHP/tail.php"); ?>
